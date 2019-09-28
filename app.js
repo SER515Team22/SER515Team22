a9f9742f8760a5161a33d2f4c01c2ddf189c1f9f
Idhant Haldankar <idhant123@gmail.com>
2019-09-29 01:37:27 +0530

    server setup complete

--- app.js
+++ app.js
@@ -2,14 +2,20 @@
 
 var express = require("express");
 var app = express();
+const calc = require('mathjs');
+
+app.use(express.json()) 
+
 app.listen(3000, () => {
  console.log("Server running on port 3000");
 });
 
-app.post("/save", (req, res, next) => {
-    res.json();
+app.get('/', (req,res, next) => {
+    res.send('reached root');
+})
+app.post("/evaluate", (req, res, next) => {
+    var data = {"response": ""}
+    let x = calc.evaluate((req.body.exp)).toString();
+    data['response'] = x;
+    res.send(data);
    });
-
-app.put('', );
-
-app.delete('', );
