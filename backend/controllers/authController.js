157bc219e1d2ac164ff088f91e61808056f8b018
Idhant Haldankar <idhant123@gmail.com>
2019-10-29 07:51:12 +0530

    needs login update

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,29 +1,41 @@
-var User = require('./models/user');
-var cookieParser = require('cookie-parser');
-var session = require('express-session');
-var passport = require('passport');
 
+const Users = require('../models/user');
 
-
+// Set up mongoose connection
+const mongoose = require('mongoose');
+mongoose.set('useNewUrlParser', true);
+let dev_db_url = 'mongodb+srv://ihaldank:idhantqwe@cluster0-qjx9o.mongodb.net/test?retryWrites=true&w=majority';
+let mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 exports.register = (req, res) => {
-    var password = req.body.password;
-    var password2 = req.body.password2;
-  
-    if (password == password2){
-      var newUser = new User({
-        name: req.body.name,
-        email: req.body.email,
-        username: req.body.username,
-        password: req.body.password
-      });
-  
-      User.createUser(newUser, function(err, user){
-        if(err) throw err;
-        res.send(user).end()
-      });
-    } else{
-      res.status(500).send("{errors: \"Passwords don't match\"}").end()
-    }
-  };
+    let user = new Users(
+        {
+          type: req.body.type,
+          username: req.body.username,
+          email: req.body.email,
+          hash: req.body.password,
+        }
+        
+    );
+    user.setPassword(req.body.password);
+    user.save((err) => {
+      if (err){
+        res.send({"status": "Not registered"});
+      }
+      res.send({"status": "Registered"});
+    })
+ 
+}
 
+exports.login = (req, res) => {
+  // var users = new Users();
+  var users = mongoose.model('User');
+  console.log(users.findOne({username: "idhant96"}, 'password'));
+  // let user = new Users();
+  // user.validPassword(req.body.password);
+  // res.send(user.findOne());
+}
