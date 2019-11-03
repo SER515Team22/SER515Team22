2333b74bc2837a3adcf57e7ec1ec13a1644ea7f3
Gayathri95 <s.gayu95@gmail.com>
2019-11-03 11:42:10 -0700

    changes to route

--- backend/routes/web.js
+++ backend/routes/web.js
@@ -6,4 +6,5 @@ const expression_controller = require('../controllers/controller');
 
 router.get('/', expression_controller.root);
 router.post('/evaluate',  expression_controller.evaluate);
+router.post('/postquestion', expression_controller.postquestion);
 module.exports = router;
